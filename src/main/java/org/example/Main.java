8f275461cb84d943d019a3ddab7f4b3e26ba046b
Benjamin Sepulveda Flores <benjamin.sepulveda@usach.cl>
2023-11-29 22:18:40 -0300

    3er commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+//a
 public class Main {
     public static void main(String[] args) {
         List<String> keywords = new ArrayList<>();
@@ -41,7 +40,7 @@ public class Main {
         chatbot.chatbotAddFlow(flow);
         chatbot.chatbotAddFlow(flow2);
         chatbot.chatbotAddFlow(flow3);
-
+        chatbot.chatbotAddFlow(flow3);
 
         System.out.println(chatbot);
 
